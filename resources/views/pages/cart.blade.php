8f63e208a30b9c418cca0816074f33aa088366f9
Nasir Lawal <nasirlawal001@gmail.com>
2020-08-21 23:14:57 +0100

    Style cart page

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -1,8 +1,12 @@
 @include('includes/topnav')
-
+    
     @if (Cart::isEmpty())
-        <h1>No items</h1>
-        @else
+        <div class="container">
+            <div class="card-body">
+                <h1>No items in your cart</h1>
+            </div>
+        </div>
+    @else
         {{-- {{ Cart::getCount() }} --}}
         <form action="{{ route('cart.empty') }}" method="GET">
             <button type="submit" class="btn btn-outline-danger mt-5 float-right mr-5">Clear Cart</button>
@@ -10,19 +14,30 @@
         
         @foreach ($cartCollection as $product)
             <div class="container m-5">
-                <div class="card-body">
+                <div class="card-body mt-2">
                     <hr>
                     <div class="row">
-                        <div class="col-md-2 col-sm-2">
-
-                        </div>
-                        <div class="col-md-6 col-sm-6">
+                        <div class="col-md-12 col-sm-12">
                             <div class="row">
                                 <div class="cols">
                                     <img src="{{ asset('images/welcome2.jpg') }}" width="80">
                                 </div>
-                                <div class="col">
-                                    <h5 class="ml-5">{{ $product->name }}</h5>
+                                <div class="col-md-5">
+                                    <h5 class="ml-3" style="color: gray">{{ $product->name }}</h5>
+                                    <p style="color: gray">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                </div>
+                                <div class="cols">
+                                    <form action="" method="GET">
+                                        <button type="submit" class="btn btn-link mr-2" style="color: gray">Remove</button>
+                                    </form>
+                                </div>
+                                <div class="col-md-2">
+                                    <form action="">
+                                        <input type="number" name="qty" class="form-control">
+                                    </form>
+                                </div>
+                                <div class="col-md-3">
+                                    NGN {{ $product->price }}
                                 </div>
                             </div>
                         </div>
@@ -32,6 +47,28 @@
             </div>
             
         @endforeach
+        <div class="container m-5">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem recusandae fugiat perspiciatis adipisci, vitae quia voluptatum itaque. Reiciendis, deserunt, fugiat quidem temporibus quam, eos distinctio ratione iusto aliquam officia inventore!
+                    </div>
+                    <div class="col-md-2">
+                        <strong class="mr-0"> Total NGN </strong> <strong class="">{{ $product->price }}</strong>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <a href="/category" class="btn btn-outline-secondary m-3">Continue Shopping</a>
+                    </div>
+                    <div class="col">
+                        <form action="" method="POST">
+                            <button type="submit" class="btn btn-outline-success m-3">Proceed to Checkout</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     @endif    
 @include('includes/footer')
